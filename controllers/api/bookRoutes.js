0e87d3ec9e02ebac315bb55b776a438ce9d28097
1122c <118850768+1122c@users.noreply.github.com>
2024-01-26 13:31:46 -0800

    added to bookRoutes.js to test open library API endpoint. endpoint is working.

--- controllers/api/bookRoutes.js
+++ controllers/api/bookRoutes.js
@@ -7,6 +7,29 @@ const { User, Book } = require("../../models");
 //   res.send("hello");
 // });
 
+//for using the OPEN LIBRARY API SEARCH
+
+router.get("/search", async (req, res) => {
+  try {
+    const searchQuery = req.query.query;
+    const response = await fetch(
+      `https://openlibrary.org/search.json?q=${encodeURIComponent(searchQuery)}`
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch from Open Library API");
+    }
+
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error("Error fetching book data:", error);
+    res
+      .status(500)
+      .json({ message: "Error fetching book data", error: error.message });
+  }
+});
+
 router.get("/:bookId", async (req, res) => {
   let book = await Book.findOne({
     where: { id: req.params.bookId },
@@ -50,10 +73,8 @@ router.delete("/:bookId", async (req, res) => {
 //book.hb id creation tbd --> just from model/book.js
 //render for select book by id
 
-//within books.hb or book.hb you want to be able to also render reviews. when i chose a book, i can also see the book reviews 
+//within books.hb or book.hb you want to be able to also render reviews. when i chose a book, i can also see the book reviews
 
 //how much more difficult is it to add the beer if we can do it
-
-
 
 module.exports = router;
