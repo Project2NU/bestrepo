f26a9e9a0989217dd9b6656cb8358e589b31421b
1122c <118850768+1122c@users.noreply.github.com>
2024-01-31 11:07:50 -0800

    added signup.hb, signup.js, edit css in login.hb

--- public/js/login.js
+++ public/js/login.js
@@ -1,26 +1,46 @@
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
-  const email = document.querySelector('#login-email').value.trim();
-  const password = document.querySelector('#login-password').value.trim();
+  const email = document.querySelector("#login-email").value.trim();
+  const password = document.querySelector("#login-password").value.trim();
 
   if (email && password) {
-    const response = await fetch('/api/users/login', {
-      method: 'POST',
+    const response = await fetch("/api/users/login", {
+      method: "POST",
       body: JSON.stringify({ email, password }),
-      headers: { 'Content-Type': 'application/json' },
+      headers: { "Content-Type": "application/json" },
     });
-  
+
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace("/");
     } else {
-      alert('Failed to log in');
+      alert("Failed to log in");
     }
   }
 };
 
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+  const name = document.querySelector("#signup-name").value.trim();
+  const email = document.querySelector("#signup-email").value.trim();
+  const password = document.querySelector("#signup-password").value.trim();
 
+  if (email && password) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({ name, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      console.log("signup failed");
+    }
+  }
+};
 
 document
-  .querySelector('.login-form')
-  .addEventListener('submit', loginFormHandler);
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
+
